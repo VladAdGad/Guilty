03694ad6d40c3a0c6aa72351813225463870969c
Vlad <vlad.gamedev@gmail.com>
2018-04-22 20:48:38 +0200

    74 created NoticeObject

---
+++ Assets/Sandbox/Vlad/LevelFlat/CommonFeature/NoticeCommonFeature/NoticeObject.cs
@@ -0,0 +1,22 @@
+﻿using LevelFlat.CommonFeature.EventManagementCommonFeature.Interface;
+using LevelFlat.CommonFeature.NoticeCommonFeature;
+using UnityEngine;
+
+public class NoticeObject : MonoBehaviour, IGazable
+{
+    private DataOfNoticeObject _dataOfNoticeObject;
+
+    private void Awake()
+    {
+        _dataOfNoticeObject = gameObject.GetComponent<DataOfNoticeObject>();
+    }
+
+    public void OnGazeEnter()
+    {
+//        _dataOfNoticeObject.ShowData();
+    }
+
+    public void OnGazeExit()
+    {
+    }
+}
