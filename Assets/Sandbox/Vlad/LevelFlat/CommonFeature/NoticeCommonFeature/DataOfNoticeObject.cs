3e97cb909df70da6d9cee1d4b7811a30ab0c29f6
Vlad <vlad.gamedev@gmail.com>
2018-04-23 02:54:48 +0200

    74 implemented of using Images in json

--- Assets/Sandbox/Vlad/LevelFlat/CommonFeature/NoticeCommonFeature/DataOfNoticeObject.cs
+++ Assets/Sandbox/Vlad/LevelFlat/CommonFeature/NoticeCommonFeature/DataOfNoticeObject.cs
@@ -1,9 +1,22 @@
-﻿namespace LevelFlat.CommonFeature.NoticeCommonFeature
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+namespace LevelFlat.CommonFeature.NoticeCommonFeature
 {
     public class DataOfNoticeObject
     {
         public string Title { get; set; }
         public string Description { get; set; }
-//        public Sprite IconOfObject { get; set; }
+        [JsonIgnore]
+        public Sprite IconOfObject { get; set; }
+
+        public string PathIcon
+        {
+            get { return PathIcon;}
+            set
+            {
+                IconOfObject = Resources.Load<Sprite>(value);
+            }
+        }
     }
 }
--- Assets/Sandbox/Vlad/LevelFlat/CommonFeature/NoticeCommonFeature/NoticeObject.cs
+++ Assets/Sandbox/Vlad/LevelFlat/CommonFeature/NoticeCommonFeature/NoticeObject.cs
@@ -1,4 +1,5 @@
-﻿using LevelFlat.CommonFeature.EventManagementCommonFeature.Interface;
+﻿using Gui;
+using LevelFlat.CommonFeature.EventManagementCommonFeature.Interface;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ namespace LevelFlat.CommonFeature.NoticeCommonFeature
     public class NoticeObject : MonoBehaviour, IGazable
     {
         [SerializeField] private TextAsset _jsonFile;
+        [SerializeField] private ImageGuiSocket _imageSocket;
         private DataOfNoticeObject _dataOfNoticeObject = new DataOfNoticeObject();
 
         private void Awake()
@@ -21,10 +23,12 @@ namespace LevelFlat.CommonFeature.NoticeCommonFeature
 
         public void OnGazeExit()
         {
+            _imageSocket.Flush();
         }
 
         private void ShowInfo()
         {
+            _imageSocket.Display(_dataOfNoticeObject.IconOfObject);
             Debug.Log(_dataOfNoticeObject.Title);
             Debug.Log(_dataOfNoticeObject.Description);
         }
