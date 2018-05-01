1c3e92c6934be1b07ffb9c105d3334379f1e8e98
Vlad <vlad.gamedev@gmail.com>
2018-05-02 00:05:23 +0200

    cleaaned prefabs and features

---
+++ Assets/Sandbox/Vlad/LevelFlat/Features/CommonFeature/NoticeFeature/NoticeClueRecordFactory.cs
@@ -0,0 +1,13 @@
+﻿using LevelFlat.CommonFeature.NoticeCommonFeature;
+using UnityEngine;
+
+namespace Sandbox.Vlad.LevelFlat.CommonFeature.NoticeCommonFeature
+{
+    public class NoticeClueRecordFactory
+    {
+        public static NoticedClueRecord Create(string title, string description, string path)
+        {
+            return new NoticedClueRecord(title, description, Resources.Load<Sprite>(path));
+        }
+    }
+}
---
+++ Assets/Sandbox/Vlad/LevelFlat/Features/CommonFeature/NoticeFeature/NoticeObject.cs
@@ -0,0 +1,39 @@
+﻿using Gui;
+using LevelFlat.CommonFeature.EventManagementCommonFeature.Interface;
+using Newtonsoft.Json;
+using Sandbox.Vlad.LevelFlat.CommonFeature.NoticeCommonFeature;
+using UnityEngine;
+
+namespace LevelFlat.CommonFeature.NoticeCommonFeature
+{
+    public class NoticeObject : MonoBehaviour, IGazable
+    {
+        [SerializeField] private TextAsset _jsonFile;
+        [SerializeField] private ImageGuiSocket _imageSocket;
+        private NoticedClueRecord _noticedClueRecord;
+
+        private void Awake()
+        {
+            JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings();
+            jsonSerializerSettings.Converters.Add(new NoticeClueRecordConventer());
+            _noticedClueRecord = JsonConvert.DeserializeObject<NoticedClueRecord>(_jsonFile.text, jsonSerializerSettings);
+        }
+
+        public void OnGazeEnter()
+        {
+            ShowInfo();
+        }
+
+        public void OnGazeExit()
+        {
+            _imageSocket.Flush();
+        }
+
+        private void ShowInfo()
+        {
+            _imageSocket.Display(_noticedClueRecord.Icon);
+            Debug.Log(_noticedClueRecord.Title);
+            Debug.Log(_noticedClueRecord.Description);
+        }
+    }
+}
