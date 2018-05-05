350232e8bf1f49b01f12b73e91e20c4142512773
Vlad <vlad.gamedev@gmail.com>
2018-05-05 20:41:02 +0200

    97 created structure for doorSystem

--- Assets/Sandbox/Dariia/LevelFlat/Features/DoorSystemFeature/DoorBehaviour.cs
+++ Assets/Sandbox/Dariia/LevelFlat/Features/DoorSystemFeature/DoorBehaviour.cs
@@ -1,42 +1,37 @@
 ﻿using LevelFlat.Features.Feature.InteractWithObjects;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace InteractableObjects.Doors
 {
     public class DoorBehaviour : Interactable
     {
-        [SerializeField] protected KeyCode ActivationButton = KeyCode.Mouse1;
         [SerializeField] private AudioSource _closingDoorAudioSource;
         [SerializeField] private AudioSource _openingDoorAudioSource;
         [SerializeField] private Animator _animator;
-
-        public bool IsDoorClosed { get; private set; } = true;
+        [SerializeField] private bool _isDoorClosed = true;
 
         public override void OnPress()
         {
-            if (IsDoorClosed)
+            if (_isDoorClosed)
                 OpenDoor();
             else
                 CloseDoor();
+
+            ChangeStateOfDoor();
         }
 
         private void OpenDoor()
         {
             _animator.SetBool("isOpen", true);
-            _openingDoorAudioSource.Play();
-
-            IsDoorClosed = false;
+//            _openingDoorAudioSource.Play();
         }
 
         public void CloseDoor()
         {
             _animator.SetBool("isOpen", false);
-            _closingDoorAudioSource.Play();
-
-            IsDoorClosed = true;
+//            _closingDoorAudioSource.Play();
         }
 
-        private void OnValidate() => Assert.AreNotEqual(ActivationButton, KeyCode.None, "Door Actiation button must not be null.");
+        private void ChangeStateOfDoor() => _isDoorClosed = !_isDoorClosed;
     }
 }
