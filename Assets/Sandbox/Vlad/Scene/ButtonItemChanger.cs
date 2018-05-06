697fd355ba4aac788af5981f06caef454e828389
Vlad <vlad.gamedev@gmail.com>
2018-05-06 13:22:41 +0200

    99 created ButtonItemChanger

---
+++ Assets/Sandbox/Vlad/Scene/ButtonItemChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonItemChanger : MonoBehaviour
+{
+    public string name;
+    public string description;
+
+    private GameObject nameObject;
+    private GameObject descriptionObject;
+
+    private Text nameText;
+    private Text descriptionText;
+
+    private void Start()
+    {
+        nameObject = GameObject.Find("Name");
+        descriptionObject = GameObject.Find("Description");
+        nameText = nameObject.GetComponent<Text>();
+        descriptionText = descriptionObject.GetComponent<Text>();
+    }
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(SetNameAndDescription);
+    }
+
+    private void SetNameAndDescription()
+    {
+        nameText.text = name;
+        descriptionText.text = description;
+    }
+}
