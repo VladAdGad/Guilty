83e3a1c854d99fa0bbf6448803a8794db77faab7
Vlad <vlad.gamedev@gmail.com>
2018-05-06 14:55:33 +0200

    99 cleaned Vlad' sandbox

--- Assets/Sandbox/Vlad/LevelFlat/Features/Feature/NoteManagerFeature/Bookmark/Evidence/Conventer/DataEvidenceFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Sandbox.Vlad.LevelFlat.Features.Feature.NoteManagerFeature.Bookmark.Evidence.Conventer
-{
-    public abstract class DataEvidenceFactory
-    {
-        public static DataEvidence Create(string title, string description, string involved, string pathIcon) => new DataEvidence(title, description, involved, Resources.Load<Sprite>(pathIcon));
-    }
-}
