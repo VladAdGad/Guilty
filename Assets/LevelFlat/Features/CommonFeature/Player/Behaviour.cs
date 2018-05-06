2cd3c1bf18e35b68860950ab986aeb68da00bfb5
Vlad <vlad.gamedev@gmail.com>
2018-05-06 21:33:21 +0200

    fixed bug with ui but unity have bug with pointer system

--- Assets/LevelFlat/Features/CommonFeature/Player/Behaviour.cs
+++ Assets/LevelFlat/Features/CommonFeature/Player/Behaviour.cs
@@ -6,17 +6,22 @@ namespace LevelFlat.Features.CommonFeature.Player
     public class Behaviour : MonoBehaviour
     {
         private GameObject _player;
-        
+        private int _disablePlayer = 0;
+        private const int CanEnable = 0;
+
         private void Start() => _player = gameObject;
 
         public void DisableFirstPersonController()
         {
+            ++_disablePlayer;
             EnableCursor();
             _player.GetComponent<FirstPersonController>().enabled = false;
         }
         
         public void EnableFirstPersonController()
         {
+            --_disablePlayer;
+            if(_disablePlayer.Equals(CanEnable))
             _player.GetComponent<FirstPersonController>().enabled = true;
         }
 
