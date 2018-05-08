3c706747459ef5ef6e52c68881ed89dfd2b10e36
Vlad <vlad.gamedev@gmail.com>
2018-05-08 19:48:44 +0200

    created page changer

---
+++ Assets/LevelFlat/Features/Feature/NotebookFeature/PageChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LevelFlat.Features.Feature.NotebookFeature
+{
+    public class PageChanger : MonoBehaviour
+    {
+        [SerializeField] private Button _evidenceBookmark;
+        [SerializeField] private Button _inventoryBookMark;
+
+        [SerializeField] private GameObject _evidencePage;
+        [SerializeField] private GameObject _inventoryPage;
+
+        private void Start()
+        {
+            _evidenceBookmark.onClick.AddListener(ActivateEvidencePage);
+            _inventoryBookMark.onClick.AddListener(ActiveInventoryPage);
+        }
+
+        private void ActivateEvidencePage() => _evidencePage.transform.SetAsLastSibling();
+
+        private void ActiveInventoryPage() => _inventoryPage.transform.SetAsLastSibling();
+    }
+}
