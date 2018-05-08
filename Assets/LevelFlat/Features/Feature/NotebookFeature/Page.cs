315a23ef159e99b029988736a4c1b7c58e1df271
Vlad <vlad.gamedev@gmail.com>
2018-05-08 20:34:15 +0200

    109 reduced code

---
+++ Assets/LevelFlat/Features/Feature/NotebookFeature/Page.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LevelFlat.Features.Feature.NotebookFeature
+{
+    public class Page : MonoBehaviour
+    {
+        [SerializeField] private Button _bookmark;
+
+        public void Init() => _bookmark.onClick.AddListener(() => gameObject.transform.SetAsLastSibling());
+    }
+}
--- Assets/LevelFlat/Features/Feature/NotebookFeature/PageChanger.cs
+++ Assets/LevelFlat/Features/Feature/NotebookFeature/PageChanger.cs
@@ -1,24 +1,12 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace LevelFlat.Features.Feature.NotebookFeature
 {
     public class PageChanger : MonoBehaviour
     {
-        [SerializeField] private Button _evidenceBookmark;
-        [SerializeField] private Button _inventoryBookMark;
+        [SerializeField] private List<Page> _pages;
 
-        [SerializeField] private GameObject _evidencePage;
-        [SerializeField] private GameObject _inventoryPage;
-
-        private void Start()
-        {
-            _evidenceBookmark.onClick.AddListener(ActivateEvidencePage);
-            _inventoryBookMark.onClick.AddListener(ActiveInventoryPage);
-        }
-
-        private void ActivateEvidencePage() => _evidencePage.transform.SetAsLastSibling();
-
-        private void ActiveInventoryPage() => _inventoryPage.transform.SetAsLastSibling();
+        private void Start() => _pages.ForEach(page => page.Init());
     }
 }
