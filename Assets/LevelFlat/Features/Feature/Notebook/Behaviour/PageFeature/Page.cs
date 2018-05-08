a1d11909caa090ea3e10d294c72cbea7f00722e7
Vlad <vlad.gamedev@gmail.com>
2018-05-09 01:39:25 +0200

    109 fixed sound repeatable

--- Assets/LevelFlat/Features/Feature/Notebook/Behaviour/PageFeature/Page.cs
+++ Assets/LevelFlat/Features/Feature/Notebook/Behaviour/PageFeature/Page.cs
@@ -6,7 +6,20 @@ namespace LevelFlat.Features.Feature.NotebookFeature
     public class Page : MonoBehaviour
     {
         [SerializeField] private Button _bookmark;
+        [SerializeField] private AudioSource _audioSource;
 
-        public void Init() => _bookmark.onClick.AddListener(() => gameObject.transform.SetAsLastSibling());
+        private const int LastSibling = 1;
+
+        public void Init()
+        {
+            _bookmark.onClick.AddListener(PlaySound);
+            _bookmark.onClick.AddListener(() => gameObject.transform.SetAsLastSibling());
+        }
+
+        private void PlaySound()
+        {
+            if (!gameObject.transform.GetSiblingIndex().Equals(LastSibling))
+                _audioSource.Play();
+        }
     }
 }
