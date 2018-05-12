5d721ca96d55ccc823f6278e1e83df9f2db11947
Vlad <vlad.gamedev@gmail.com>
2018-05-12 16:16:54 +0200

    112 created example of zenject

---
+++ Assets/Sandbox/Vlad/GameInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using LevelFlat.Features.Feature.NotebookFeature;
+using UnityEngine;
+using Zenject;
+using Zenject.SpaceFighter;
+
+namespace DefaultNamespace
+{
+    public class GameInstaller: MonoInstaller
+    {
+        [Inject] private Settings _settings = null;
+
+        public override void InstallBindings()
+        {
+//            Container.Bind<ITickable>().To<NotebookManager>().AsSingle();
+            Container.Bind<NotebookManager>().AsSingle().WithArguments(_settings.Player).NonLazy();
+        }
+        
+        [Serializable]
+        public class Settings
+        {
+            public GameObject Player;
+        }
+    }
+}
---
+++ Assets/Sandbox/Vlad/GameSettingsInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "GameSettingsInstaller", menuName = "Installers/GameSettingsInstaller")]
+    public class GameSettingsInstaller: ScriptableObjectInstaller<GameSettingsInstaller>
+    {
+        public GameInstaller.Settings GameInstaller;
+        
+        public override void InstallBindings()
+        {
+            Container.BindInstance(GameInstaller);
+        }
+    }
+}
