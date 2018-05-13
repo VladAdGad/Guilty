d4e88ff8760965b5feda0e1f666cb0ec0891c420
Vlad <vlad.gamedev@gmail.com>
2018-05-13 23:40:29 +0200

    119 step 1

--- Assets/Sandbox/Vlad/GameInstaller.cs
+++ Assets/Sandbox/Vlad/GameInstaller.cs
@@ -1,15 +1,38 @@
-﻿using LevelFlat.Features.CommonFeature.Player;
+﻿using System;
+using LevelFlat.Features.CommonFeature.Player;
+using LevelFlat.Features.Feature.InteractWithObjects.ExaminationSystemFeature.Scripts.GUI;
 using LevelFlat.Features.Feature.NoteManagerFeature.Bookmark;
+using UnityStandardAssets.Characters.FirstPerson;
 using Zenject;
 
 namespace DefaultNamespace
 {
     public class GameInstaller : MonoInstaller
     {
+        public PlayerSettings PlayerSettingsScene;
+        public GuiSettings GuiSettingsScene;
+        
         public override void InstallBindings()
         {
             Container.Bind<PlayerBehaviour>().AsSingle();
             Container.Bind<DataTaskProxy>().AsSingle();
+            
+            Container.BindInstance(PlayerSettingsScene.FirstPersonController).AsSingle();
+            
+            Container.BindInstance(GuiSettingsScene.CrosshairManager).AsSingle();
+        }
+        
+        [Serializable]
+        public class PlayerSettings
+        {
+            public FirstPersonController FirstPersonController;
+            
+        }
+
+        [Serializable]
+        public class GuiSettings
+        {
+            public CrosshairManager CrosshairManager;
         }
     }
 }
