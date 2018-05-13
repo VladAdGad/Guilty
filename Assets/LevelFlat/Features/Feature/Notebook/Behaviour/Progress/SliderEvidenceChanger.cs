dac36e76118fb1eaf068fb3e95bd59a0ba5d23fe
Vlad <vlad.gamedev@gmail.com>
2018-05-13 16:22:09 +0200

    fixed bug???? with nonames

--- Assets/LevelFlat/Features/Feature/Notebook/Behaviour/Progress/SliderEvidenceChanger.cs
+++ Assets/LevelFlat/Features/Feature/Notebook/Behaviour/Progress/SliderEvidenceChanger.cs
@@ -7,7 +7,7 @@ namespace LevelFlat.Features.Feature.NotebookFeature
 {
     public class SliderEvidenceChanger : MonoBehaviour
     {
-        private string _involveName = "";
+        [SerializeField] private string _involveName = "";
         private Text _amountOfEvidences;
         private Slider _slider;
         private readonly List<DataEvidence> _dataEvidences = new List<DataEvidence>();
@@ -28,7 +28,6 @@ namespace LevelFlat.Features.Feature.NotebookFeature
 
         private void InitComponents()
         {
-            _involveName = GetComponent<Text>().text;
             _amountOfEvidences = transform.GetChild(1).GetComponentInChildren<Text>();
             _slider = GetComponentInChildren<Slider>();
         }
