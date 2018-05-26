94ad63a8dac728dac2870dc2dc570cad6e84e46d
Vlad <vlad.gamedev@gmail.com>
2018-05-26 14:53:56 +0200

    135 created prototype of load scene

---
+++ Assets/Sandbox/Vlad/BetweenScenes/LevelChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    private Animator _animator;
+    private int _loadLevel;
+    private const int NextScene = 1;
+
+    private void Start() => _animator = GetComponent<Animator>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        _loadLevel = levelIndex;
+        _animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(_loadLevel);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + NextScene);
+    }
+}
