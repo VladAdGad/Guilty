80d1a3b729b08b9cd74c3de8fb1b660e948f605d
Vlad <vlad.gamedev@gmail.com>
2018-05-28 14:17:51 +0200

    143 created radio feature

---
+++ Assets/Sandbox/Vlad/RadioExample/RadioBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using LevelFlat.Features.Feature.InteractWithObjects;
+
+namespace UnityEngine
+{
+    public class RadioBehaviour : Interactable
+    {
+        private AudioSource _recordingAudio;
+        private bool _stateOfRadio;
+
+        private void Start() => _recordingAudio = GetComponent<AudioSource>();
+
+        public override void OnPress()
+        {
+            if (_stateOfRadio)
+                StopPlayRecording();
+            else
+                StartPlayRecording();
+
+            ChangeStateOfRadio();
+        }
+
+        private void StartPlayRecording() => _recordingAudio.Play();
+        private void StopPlayRecording() => _recordingAudio.Stop();
+
+        private void ChangeStateOfRadio() => _stateOfRadio = !_stateOfRadio;
+    }
+}
