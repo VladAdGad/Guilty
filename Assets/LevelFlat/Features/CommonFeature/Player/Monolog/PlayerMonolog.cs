2fd6dea6021a72bb1fb99b9b54b74556da990cce
Vlad <vlad.gamedev@gmail.com>
2018-05-28 13:44:01 +0200

    147 added subtitles feature to monolog

--- Assets/LevelFlat/Features/CommonFeature/Player/Monolog/PlayerMonolog.cs
+++ Assets/LevelFlat/Features/CommonFeature/Player/Monolog/PlayerMonolog.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
+using Zenject;
 
 namespace LevelFlat.Features.CommonFeature.Player.Monolog
 {
     public class PlayerMonolog : MonoBehaviour
     {
         [SerializeField] private AudioSource _monologSound;
+        [SerializeField] private string _textOfSubtitlesMonolog;
+        [Inject] private SubtitlesMonolog _subtitlesMonolog;
 
-        public void PlayMonolog() => _monologSound.Play();
+        public void StartMonolog()
+        {
+            _monologSound?.Play();
+            _subtitlesMonolog.ShowSubtitles(_textOfSubtitlesMonolog);
+        }
     }
 }
