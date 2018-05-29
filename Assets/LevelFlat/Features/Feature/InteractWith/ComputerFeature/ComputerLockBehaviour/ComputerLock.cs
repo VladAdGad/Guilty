629b319cc448add9d6437cf825b7177f6daa0058
Vlad <vlad.gamedev@gmail.com>
2018-05-29 11:41:54 +0200

    142 created example of lock door and computer

---
+++ Assets/LevelFlat/Features/Feature/InteractWith/ComputerFeature/ComputerLockBehaviour/ComputerLock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LevelFlat.Features.Feature.InteractWithObjects;
+using LevelFlat.Features.Feature.Notebook.NoteCollectionManagerFeature.Bookmark.Evidence;
+using UnityEngine;
+
+namespace LevelFlat.Features.Feature.InteractWith.ComputerFeature.ComputerLockBehaviour
+{
+    public class ComputerLock : Interactable
+    {
+        [SerializeField] private string _lockStateTooltip;
+        [SerializeField] private List<EvidenceEntity> _evidenceEntity;
+        private bool _isUnlocked;
+
+        public override void OnGazeEnter() => ShowInfoOfSeenObject(_isUnlocked ? ItemName : _lockStateTooltip);
+
+        public override void OnPress()
+        {
+            if (!_isUnlocked) return;
+            UpdateNotebook();
+
+            Destroy(this);
+        }
+
+        public void Unlock() => _isUnlocked = !_isUnlocked;
+
+        private void UpdateNotebook() => _evidenceEntity?.ForEach(it => it.AddEvidence());
+    }
+}
---
+++ Assets/LevelFlat/Features/Feature/InteractWith/ComputerFeature/ComputerLockBehaviour/ComputerUnlock.cs
@@ -0,0 +1,16 @@
+﻿using LevelFlat.Features.Feature.InteractWith.PickupSystemFeature;
+using UnityEngine;
+
+namespace LevelFlat.Features.Feature.InteractWith.ComputerFeature.ComputerLockBehaviour
+{
+    public class ComputerUnlock : PickupObject
+    {
+        [SerializeField] private ComputerLock _computerLock;
+        
+        public override void OnPress()
+        {
+            base.OnPress();
+            _computerLock.Unlock();
+        }
+    }
+}
