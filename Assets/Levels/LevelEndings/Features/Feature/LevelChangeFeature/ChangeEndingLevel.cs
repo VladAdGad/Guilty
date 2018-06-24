cd801662737d920952b8a4eef8d3c797b1947fdd
Vlad <vlad.gamedev@gmail.com>
2018-06-24 20:10:23 +0200

    261 created script for endings

---
+++ Assets/Levels/LevelEndings/Features/Feature/LevelChangeFeature/ChangeEndingLevel.cs
@@ -0,0 +1,16 @@
+﻿using CommonFeature.LevelChange;
+using UnityEngine;
+using Zenject;
+
+namespace Levels.LevelEndings.Features.Feature.LevelChangeFeature
+{
+	public class ChangeEndingLevel : MonoBehaviour {
+
+		public class LevelCreditsChanger : MonoBehaviour
+		{
+			[Inject] private LevelChanger _levelChanger;
+
+			public void ChangeLevel() => StartCoroutine(_levelChanger.LoadIndexScene(SceneIndex.Credits));
+		}
+	}
+}
