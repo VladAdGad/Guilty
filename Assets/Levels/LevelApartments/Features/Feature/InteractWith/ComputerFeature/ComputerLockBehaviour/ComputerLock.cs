6ac3d9044a6dd309cd209b49c4b0b9ab4110b08a
Vlad <vlad.gamedev@gmail.com>
2018-06-25 10:43:53 +0200

    194 прочитал мысли Даши :D

--- Assets/Levels/LevelApartments/Features/Feature/InteractWith/ComputerFeature/ComputerLockBehaviour/ComputerLock.cs
+++ Assets/Levels/LevelApartments/Features/Feature/InteractWith/ComputerFeature/ComputerLockBehaviour/ComputerLock.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Levels.LevelApartments.Features.Feature.Notebook.NoteCollectionManagerFeature.Bookmark.Evidence;
+using Levels.LevelApartments.Features.Feature.Notebook.NoteCollectionManagerFeature.Bookmark.Task;
 using UnityEngine;
 
 namespace Levels.LevelApartments.Features.Feature.InteractWith.ComputerFeature.ComputerLockBehaviour
@@ -8,9 +9,17 @@ namespace Levels.LevelApartments.Features.Feature.InteractWith.ComputerFeature.C
     {
         [SerializeField] private string _lockStateTooltip;
         [SerializeField] private List<EvidenceEntity> _evidenceEntity;
+        [SerializeField] private CreateTaskEntity _createTaskEntity;
         private bool _isUnlocked;
 
-        public override void OnGazeEnter() => ShowInfoOfSeenObject(_isUnlocked ? ItemName : _lockStateTooltip);
+        private void Start() => _createTaskEntity = GetComponent<CreateTaskEntity>();
+
+        public override void OnGazeEnter()
+        {
+            if (_createTaskEntity != null)
+                _createTaskEntity.AddTask();
+            ShowInfoOfSeenObject(_isUnlocked ? ItemName : _lockStateTooltip);
+        }
 
         public override void OnPress()
         {
